fcf534ce4393b48e7f74fc4857b72af860992ebb
Erik Ivan Retana Lopez <merzelert@gmail.com>
2022-08-13 17:34:27 -0500

    landing

--- src/assets/main.js
+++ src/assets/main.js
@@ -32,7 +32,7 @@ async function fetchData(urlApi) {
             </h3>
         </div>
         </div>
-        `).slice(0,12).join('')}
+        `).slice(0,40).join('')}
     `;
     content.innerHTML = view;
     } catch (error){
